cf62484a195ec973f36847606bd32cbb8cbab0be
feelipechs <flpchagas@outlook.com>
2025-10-17 19:50:44 -0300

    removendo coisas que esqueci antes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MonetaryDonations from './pages/MonetaryDonations';
-import Courses from './pages/Courses';
 import Campaigns from './pages/Campaigns';
 import Help from './pages/Help';
 import Profile from './pages/Profile';
@@ -17,7 +16,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registrar" element={<Register />} />
-        <Route path="/cursos" element={<Courses />} />
         <Route path="/campanhas" element={<Campaigns />} />
         <Route path="/doacoes-monetarias" element={<MonetaryDonations />} />
         <Route path="/ajuda" element={<Help />} />
